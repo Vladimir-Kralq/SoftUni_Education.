735d58b4a48eca6eba5824a99775e35361834799
Vladimir Djovanov <stu2401681022@uni-plovdiv.bg>
2025-10-16 02:37:26 +0300

    Delete Programing Basic with JS/04. Password Guess.js

--- Programing Basic with JS/04. Password Guess.js
+++
@@ -1,9 +0,0 @@
-function passwordGuess(pass) {
-  if (pass === "s3cr3t!P@ssw0rd") {
-    console.log("Welcome");
-  } else {
-    console.log("Wrong password!");
-  }
-}
-
-passwordGuess("s3cr3t!P@ssw0rd");
