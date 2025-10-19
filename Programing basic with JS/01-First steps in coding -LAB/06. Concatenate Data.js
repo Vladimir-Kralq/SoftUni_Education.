790501da9aee97ba59adb92e69aa65c11f423f33
Vladimir Djovanov <stu2401681022@uni-plovdiv.bg>
2025-10-19 15:31:01 +0300

    Add files via upload

---
+++ Programing basic with JS/01-First steps in coding -LAB/06. Concatenate Data.js
@@ -0,0 +1,14 @@
+function getData (firstName,lastName,age ,town) {
+
+console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+
+
+
+
+
+
+
+
+}
+
+getData("Vladi", "Dzhovanov",20,"Plovdiv")
---
+++ Programing basic with JS/01-First steps in coding -LAB/07. Projects Creation.js
@@ -0,0 +1,20 @@
+function createProject(architect,broi) {
+
+let name =architect;
+let projects=broi;
+
+let projectsTIme=3;
+let finish = projects * projectsTIme;
+
+console.log(`The architect ${architect} will need ${finish} hours to complete ${projects} project/s.`);
+
+
+
+
+
+
+
+
+
+}
+createProject("George",4);
