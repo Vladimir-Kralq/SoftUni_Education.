7058b842df52001995efcce2f6df8272b7c384b3
Vladimir Djovanov <stu2401681022@uni-plovdiv.bg>
2025-10-27 19:38:37 +0200

    Add files via upload

---
+++ Programing basic with JS/For Loops-LAB/01. Numbers from 1 to 100.js
@@ -0,0 +1,10 @@
+function numbers() {
+for(let i=1; i<=100 ;i+=1)
+{
+console.log(i);
+
+}
+
+
+}
+numbers();
---
+++ Programing basic with JS/For Loops-LAB/02. Numbers N...1.js
@@ -0,0 +1,14 @@
+function num (n){
+for(let i=n ; i>=1; i-=1)
+{
+console.log(i);
+
+}
+
+
+
+
+
+}
+
+num(2);
