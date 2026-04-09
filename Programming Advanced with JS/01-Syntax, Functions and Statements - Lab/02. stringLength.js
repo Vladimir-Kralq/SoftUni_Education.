2c955be4e021663da826d4347dbe4fd7be854c2d
Vladimir Djovanov <stu2401681022@uni-plovdiv.bg>
2026-04-10 01:33:23 +0300

    Add files via upload

---
+++ Programming Advanced with JS/01-Syntax, Functions and Statements - Lab/02. stringLength.js
@@ -0,0 +1,16 @@
+function stringLength(str1, str2, str3){
+
+let strLenghtSum=str1.length+str2.length+str3.length;
+let strAverageSum= Math.floor(strLenghtSum/3);
+
+console.log(strLenghtSum);
+console.log(strAverageSum);
+
+
+
+
+
+
+
+}
+stringLength('chocolate', 'ice cream', 'cake');
